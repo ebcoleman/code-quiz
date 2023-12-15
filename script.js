6683bdc95cf4a375925d360e8ec68fcc686f6cbf
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2023-12-15 12:42:13 -0500

    working on the functionality of starting the quiz, questions do not appear yet

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 var startButton = document.getElementById("start-btn");
+var startScreen = document.getElementById("start-screen");
+var quiz = document.getElementById("quiz");
+var questionElement = document.getElementById("question");
+var choicesElement = document.getElementById("choices");
+
+var questions = [
+    {
+        question: "What is JavaScript?",
+        choices:["A coffee shop","Gives the webpage style","Allows user to interact with webpage"],
+        answer: "Allows user to interact with webpage"
+    },
+    {
+        question: "What is the index of the initial element in a JavaScript array?",
+        choices: ["0","1","A or B"],
+        answer: "0"
+    },
+    {
+        question: "Is Java and JavaScript the samething?",
+        choices: ["Duh, of course", "No way!", "Maybe?"],
+        answer: "No way!"
+    }
+]
+
 
 function startButtonClick() {
-    console.log("Clicked start button")
-    // add logic once start is clicked, which would be to start timer and display question
+    console.log("Started quiz")
+    // once start button is clicked this hides the start screen
+    startScreen.style.display = "none";
+
+    quiz.style.display = "block";
+    
+
 }
 
 startButton.addEventListener("click", startButtonClick);
 
-// var questions = [
-//     {
-//         question: "What is JavaScript?",
-//         choices:["A coffee shop","Gives the webpage style","Allows user to interact with webpage"],
-//         answer: "Allows user to interact with webpage"
-//     },
-//     {
-//         question: "What is the index of the initial element in a JavaScript array?",
-//         choices: ["0","1","A or B"],
-//         answer: "0"
-//     },
-//     {
-//         question: "Is Java and JavaScript the samething?",
-//         choices: ["Duh, of course", "No way!", "Maybe?"],
-//         answer: "No way!"
-//     }
-// ]
