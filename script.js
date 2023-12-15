4dce7603f0d5135750807472b9ea3caacb2c9580
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2023-12-15 12:11:42 -0500

    got the start button to log the click

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-var questions = [
-    {
-        question: "What is JavaScript?",
-        choices:["A coffee shop","Gives the webpage style","Allows user to interact with webpage"],
-        answer: "Allows user to interact with webpage"
-    },
-    {
-        question: "What is the index of the initial element in a JavaScript array?",
-        choices: ["0","1","A or B"],
-        answer: "0"
-    },
-    {
-        question: "Is Java and JavaScript the samething?",
-        choices: ["Duh, of course", "No way!", "Maybe?"],
-        answer: "No way!"
-    }
-]
+var startButton = document.getElementById("start-btn");
+
+function startButtonClick() {
+    console.log("Clicked start button")
+    // add logic once start is clicked, which would be to start timer and display question
+}
+
+startButton.addEventListener("click", startButtonClick);
+
+// var questions = [
+//     {
+//         question: "What is JavaScript?",
+//         choices:["A coffee shop","Gives the webpage style","Allows user to interact with webpage"],
+//         answer: "Allows user to interact with webpage"
+//     },
+//     {
+//         question: "What is the index of the initial element in a JavaScript array?",
+//         choices: ["0","1","A or B"],
+//         answer: "0"
+//     },
+//     {
+//         question: "Is Java and JavaScript the samething?",
+//         choices: ["Duh, of course", "No way!", "Maybe?"],
+//         answer: "No way!"
+//     }
+// ]
