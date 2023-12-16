ffaeacf32ab5080c2a993392824be0902d303353
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2023-12-16 11:25:09 -0500

    starting writing code to make the answer buttons work, and the the users selection dictates a right or wrong answer.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var quiz = document.getElementById("quiz");
 var questionElement = document.getElementById("question");
 var choicesElement = document.getElementById("choices");
 
+
 var questions = [
     {
         question: "What is JavaScript?",
@@ -31,6 +32,31 @@ function askQuestion(){
     document.getElementById("choiceB").textContent = currentQuestion.choices[1];
     document.getElementById("choiceC").textContent = currentQuestion.choices[2];
 };
+// lines 36-44 are stating that when that choice is clicked, it is in relation to that specific answer in the array; then it will call the function to handle the user's answer
+document.getElementById("choiceA").addEventListener("click", function (){
+    handleUserAnswer(currentQuestion.choices[0])
+});
+document.getElementById("choiceB").addEventListener("click", function (){
+    handleUserAnswer(currentQuestion.choices[1])
+});
+document.getElementById("choiceC").addEventListener("click", function (){
+    handleUserAnswer(currentQuestion.choices[2])
+});
+
+function handleUserAnswer(userChoice) {
+    var currentQuestion = questions[questionIndex]; 
+
+    if (userChoice === currentQuestion.answer) {
+        console.log("CORRECT ANSWER!")
+    }
+    else {
+        console.log("INCORRECT ANSWER! =/(")
+    }
+
+    questionIndex++;
+
+    askQuestion();
+}
 
 function startButtonClick() {
     console.log("Started quiz")
